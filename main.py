cfa9ed8b4728486681b14cecc5c767e6fde413fb
Coolkid6999 <83922972+Coolkid6999@users.noreply.github.com>
2022-07-11 08:17:52 +0100

    Add files via upload
    database needed for truecord

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
-import requests
-import json
-import zipfile
+from flask import Flask, redirect
 import os
-#import subprocess
-EXTERNAL = requests.get("https://truecord-database.thechadcoders.repl.co").json()
-LOCAL = json.load(open("main.json", "r"))
-EXTERNALver = EXTERNAL["version"]
-if EXTERNAL["version"] != LOCAL["version"]:
-    yn = input("An update is available...\nWould you like to install it?\n[Y/N]")
-    if yn == "y":
-        url = requests.get('https://truecord-database.thechadcoders.repl.co/latest').text
-        path = f"./versions/{EXTERNALver}"
-        r = requests.get(url, allow_redirects=True)
-        open(path, "wb").write(r.content)
-        with zipfile.ZipFile(path, 'r') as zip_ref:
-            os.remove(path)
-            zip_ref.extractall("./versions")
-        with open("main.json", "r") as f:
-            version = json.load(f)
-    
-        version["version"] = EXTERNALver
+app = Flask('')
 
-        with open("main.json", "w") as f:
-            version = json.dump(version, f, indent=4)
-        os.system(f"python3 {path}/main.py")
-    else:
-        
-        folder = LOCAL["version"]
-        os.system(f"python3 ./versions/{folder}/main.py")
-        #subprocess.call([f"./versions/{folder}/main.py"])
-else:
-    url = LOCAL["version"]
-    os.system(f"python3 ./versions/{url}/main.py")
+@app.after_request
+def treat_as_plain_text(response):
+    response.headers["content-type"] = "text/plain"
+    return response
+
+
+@app.route('/')
+def home():
+  return {"version": "0.2"}
+
+@app.route('/latest')
+def latest():
+    return "https://cdn.discordapp.com/attachments/844345665688698891/844345684786544640/0.2.zip"
+
+@app.route('/plugins/test')
+def plugins_test():
+    return "https://cdn.discordapp.com/attachments/834547119129624636/844272126210867240/test-tc.zip"
+
+app.run(host="0.0.0.0",port=8080)
